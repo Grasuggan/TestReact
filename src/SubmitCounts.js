cb55f0e1e2dd3c1683cd649abf08f518253ce69c
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2021-03-24 20:54:57 +0100

    submit count

--- src/SubmitCounts.js
+++ src/SubmitCounts.js
@@ -4,29 +4,29 @@ import Submitted from './Submitted.js'
 function SubmitCounts(props){
 
 
-//     let submitters;
-//     for(let i = 0; i < props.submitters.length; i ++){
-//     console.log(`In SubmitCounts component there are ${props.submitters} submitters.`)
+    let submitters;
+    for(let i = 0; i < props.submitters.length; i ++){
+    console.log(`In SubmitCounts component there are ${props.submitters} submitters.`)
 
-//         console.log(`In SubmitCounts firstname of submitted is ${props.submitters[i].firstName}.`)
-//         return ( 
-//         submitters = <div><label>Form submitted {props.inputs} times</label><div className="profile"><Submitted inputs={props.submitters[i]} /></div></div>
-//         )
+        console.log(`In SubmitCounts firstname of submitted is ${props.submitters[i].firstName}.`)
+        return ( 
+        submitters = <div><label>Form submitted {props.inputs} times</label><div className="profile"><Submitted inputs={props.submitters[i]} /></div></div>
+        )
     
-// }
+}
      
     
-        return(
-            <>
-                {
-                   props.submitters.map((submitter, count) => {
-                    <div><label>Form submitted {count} times</label><div className="profile"><Submitted inputs={submitter} /></div></div>
+    //     return(
+    //         <>
+    //             {
+    //                props.submitters.map((submitter, count) => {
+    //                 <div><label>Form submitted {count} times</label><div className="profile"><Submitted inputs={submitter} /></div></div>
 
-                   }) 
-                }
-            </>
-        )
-    }
+    //                }) 
+    //             }
+    //         </>
+    //     )
+    // }
 
 // let submitters;
 // if(props.submitters.firstName !== ""){
@@ -40,12 +40,12 @@ function SubmitCounts(props){
 // }
  
 
-//     return(
-//         <>
+    return(
+        <>
        
-//             {submitters}
-//         </>
-//     )
-// }
+            {submitters}
+        </>
+    )
+}
 
 export default SubmitCounts
