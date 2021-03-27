564cd7cc2c7cd1ca9d5425ec2a6124050913984d
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2021-03-27 12:13:51 +0100

    submit button state error

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -11,67 +11,67 @@ class SubmitButton extends React.Component{
         }
     };
   
-    // renderCurrentBtn(){
-        
-    //      if(this.props.inputState.firstName !== "" && this.props.inputState.lastName !== ""){
-    //         this.setState({
-    //             btnInfo: {
-                 
-    //                 color: 'green',
-    //                 text: 'Send'
-    //             }
-    //         })
-            
-    //      }
-    //      else {
-    //         this.setState({
-    //             btnInfo: {
-    //                 color: "red",
-    //                 text: "Disabled"
-    //             }
-    //         })
-               
-    //      }
-    //      return this.state.btnInfo
-    //  }
-    //  render() {
-     
-    //     return (
-    //         <div>
-    //          <button type="submit" className={this.renderCurrentBtn().color}>{this.state.btnInfo.text}</button>
-    //         </div> 
-    //         )
-    // }
     renderCurrentBtn(){
-       let btnInfo = {
-            color: 'red',
-            text: 'Disabled'
-        }
-        if(this.props.inputState.firstName !== "" && this.props.inputState.lastName !== ""){
-            btnInfo ={
-                
-                color: 'green',
-                text: 'Send'
-            }
-        }
-        else {
-            btnInfo = {
-                color: 'red',
-                text: 'Disabled'
-            }
-              
-        }
-        return btnInfo
-    }
-
-    render() {
+        
+         if(this.props.inputState.firstName !== "" && this.props.inputState.lastName !== ""){
+            this.setState({
+                btnInfo: {
+                 
+                    color: 'green',
+                    text: 'Send'
+                }
+            })
+            
+         }
+         else {
+            this.setState({
+                btnInfo: {
+                    color: "red",
+                    text: "Disabled"
+                }
+            })
+               
+         }
+         return this.state.btnInfo
+     }
+     render() {
      
         return (
             <div>
-             <button type="submit" className={this.renderCurrentBtn().color}>{this.renderCurrentBtn().text}</button>
+             <button type="submit" className={this.renderCurrentBtn().color}>{this.state.btnInfo.text}</button>
             </div> 
             )
     }
+    // renderCurrentBtn(){
+    //    let btnInfo = {
+    //         color: 'red',
+    //         text: 'Disabled'
+    //     }
+    //     if(this.props.inputState.firstName !== "" && this.props.inputState.lastName !== ""){
+    //         btnInfo ={
+                
+    //             color: 'green',
+    //             text: 'Send'
+    //         }
+    //     }
+    //     else {
+    //         btnInfo = {
+    //             color: 'red',
+    //             text: 'Disabled'
+    //         }
+              
+    //     }
+    //     return btnInfo
+    // }
+
+    // render() {
+     
+    //     return (
+    //         <div>
+    //          <button type="submit" className={this.renderCurrentBtn().color}>{this.renderCurrentBtn().text}</button>
+    //         </div> 
+    //         )
+    // }
 }
 
 export default SubmitButton
